ab59cf76947610f53ddbd63764d2cdcb6c3965e2
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2022-01-21 18:09:10 -0500

    :construction: [ADD] Basic Home Page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,13 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:band_names/models/band.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<Band> _bands = [
+    Band(id: '1', name: 'Metallica', votes: 5),
+    Band(id: '2', name: 'Queen', votes: 3),
+    Band(id: '3', name: 'Iron Maiden', votes: 2),
+    Band(id: '4', name: 'Deep Purple', votes: 1),
+    Band(id: '5', name: 'Led Zeppelin', votes: 0),
+  ];
+
+  // Method for Render the Circles with the Band Names
+  ListTile _bandTile(Band band) {
+    return ListTile(
+      leading: CircleAvatar(
+        child: Text(band.name.substring(0, 2)),
+        backgroundColor: Colors.blue[100],
+      ),
+      title: Text(band.name),
+      trailing:
+          Text(band.votes.toString(), style: const TextStyle(fontSize: 20)),
+      onTap: () => {print(band.name)},
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello world ssss'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'BandNames',
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: ListView.builder(
+          //* It is important to add the length of the list rendered
+          itemCount: _bands.length,
+          itemBuilder: (context, index) => _bandTile(_bands[index])),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        elevation: 1,
+        onPressed: () {},
       ),
     );
   }
